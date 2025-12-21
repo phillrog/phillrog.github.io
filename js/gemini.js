6884dcc34ae1a4aa10d6041ade18716c28f27853
Phillipe <8622005+phillrog@users.noreply.github.com>
2025-12-21 12:25:51 -0300

    Update currentModel to latest version

--- js/gemini.js
+++ js/gemini.js
@@ -4,7 +4,7 @@ import { GoogleGenerativeAI } from "https://esm.run/@google/generative-ai";
 import { marked } from "https://esm.run/marked";    
   
 let chat;
-let currentModel = "gemini-1.5-flash";
+let currentModel = "gemini-1.5-flash-latest";
 let historico = [];
 let systemInstruction = `
 Você é o "Phill", o assistente virtual inteligente do currículo de Phillipe Roger Souza. 
