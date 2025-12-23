6e6b9f964ca1244dc9027fbd99ae9b27392cd6bd
Phillipe R Souza <8622005+phillrog@users.noreply.github.com>
2025-12-23 20:33:38 -0300

    configuração e adicção assistente

--- js/box.js
+++ js/box.js
@@ -1,22 +1,22 @@
-const x = document.getElementById("box");
-const elee5IPTd = $('.e5IPTd');
-const eleVOYwb = $('.VOYwb');
-const elelWI5Pb = $('.lWI5Pb');
-const eleXyWBof = $('.XyWBof');
-const eleEiVpKc = $('.EiVpKc');
-	
+
 function openChatBox(e) {
 	
-    
-    if (x.classList.contains('open')) {
-      x.classList.remove("open");
+    const box = $("#box");
+    const elee5IPTd = $('.e5IPTd');
+    const eleVOYwb = $('.VOYwb');
+    const elelWI5Pb = $('.lWI5Pb');
+    const eleXyWBof = $('.XyWBof');
+    const eleEiVpKc = $('.EiVpKc');
+        
+    if (box.hasClass('open')) {
+      box.removeClass("open");
 	  elee5IPTd.removeClass(`e5IPTd-forcar-hover`);
 	  eleVOYwb.removeClass(`VOYwb-forcar-hover`);
 	  elelWI5Pb.removeClass(`lWI5Pb-forcar-hover`);
 	  eleXyWBof.removeClass(`XyWBof-forcar-hover`);
 	  eleEiVpKc.removeClass(`EiVpKc-forcar-hover`);	  
     } else {
-      x.classList.add("open");
+      box.addClass("open");
 	  elee5IPTd.addClass(`e5IPTd-forcar-hover`);
 	  eleVOYwb.addClass(`VOYwb-forcar-hover`);
 	  elelWI5Pb.addClass(`lWI5Pb-forcar-hover`);
@@ -27,15 +27,20 @@ function openChatBox(e) {
   }
   
 window.addEventListener('click', function(e) {
-
+  const box = $("#box");
+  const elee5IPTd = $('.e5IPTd');
+  const eleVOYwb = $('.VOYwb');
+  const elelWI5Pb = $('.lWI5Pb');
+  const eleXyWBof = $('.XyWBof');
+  const eleEiVpKc = $('.EiVpKc');
   const btnAbrir = document.querySelector('button.VYBDae-JX-I'); // substitua pela classe/id do seu ícone de chat
 
   // Se o box estiver aberto E o clique NÃO for dentro do box E NÃO for no botão que abre
-  if (box.classList.contains('open') && 
+  if (box.hasClass('open') && 
       !box.contains(e.target) && 
       (!btnAbrir || !btnAbrir.contains(e.target))) {
     
-    box.classList.remove("open");
+    box.removeClass("open");
     elee5IPTd.removeClass(`e5IPTd-forcar-hover`);
     eleVOYwb.removeClass(`VOYwb-forcar-hover`);
     elelWI5Pb.removeClass(`lWI5Pb-forcar-hover`);
